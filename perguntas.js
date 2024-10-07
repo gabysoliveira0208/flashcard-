7254cfeeb5908697ca49aecf4b27cd1bed979aa2
gabysoliveira0208 <163007831+gabysoliveira0208@users.noreply.github.com>
2024-10-07 11:25:02 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Planetas',
+    'Qual o planeta mais frio do sistema solar?',
+    'Urano'
+)
+
+criaCartao(
+    'Carros de luxo',
+    'Em que ano a Ferrari lançou seu primeiro carro?',
+    '1947'
+)
+
+criaCartao(
+    'Grandes palavras',
+    'Qual a maior palavra do Brasil?',
+    'pneumoultramicroscopicossilicovulcanoconiótico'
+)
+
+criaCartao(
+    'Cores',
+    'Quantos tons de azul existem?',
+    'Mais de 100 tonalidades conhecidas dessa cor'
+)
+
+criaCartao(
+    'Países',
+    'Qual o país com mais pessoas ruivas no mundo?',
+    'Escócia'
+)
+
+criaCartao(
+    'Livros',
+    'Qual o livro mais lido do mundo?',
+    'A Bíblia'
+)
